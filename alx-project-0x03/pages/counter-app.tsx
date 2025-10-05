4412518cd51588b85c4becc0dfbb787b186465d5
Mistire <96515111+Mistire@users.noreply.github.com>
2025-10-05 15:17:36 +0300

    Initial commit

---
+++ alx-project-0x03/pages/counter-app.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/components/layouts/Layout";
+
+export default function CounterApp() {
+  return (
+    <Layout>
+      <div className="p-8">
+        <h1 className="text-3xl font-bold">Counter App</h1>
+        <p>This is a demo counter app page.</p>
+      </div>
+    </Layout>
+  );
+}
